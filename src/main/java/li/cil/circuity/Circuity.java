6881049e05a8a1a1d905f68e3b6378ac9165fa92
Florian Nücke <florian@nuecke.de>
2020-09-02 14:44:19 +0200

    init

---
+++ src/main/java/li/cil/circuity/Circuity.java
@@ -0,0 +1,8 @@
+package li.cil.circuity;
+
+import li.cil.circuity.api.CircuityAPI;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CircuityAPI.MOD_ID)
+public final class Circuity {
+}
---
+++ src/main/java/li/cil/circuity/api/CircuityAPI.java
@@ -0,0 +1,8 @@
+package li.cil.circuity.api;
+
+public final class CircuityAPI {
+    public static final String MOD_ID = "circuity";
+
+    private CircuityAPI() {
+    }
+}

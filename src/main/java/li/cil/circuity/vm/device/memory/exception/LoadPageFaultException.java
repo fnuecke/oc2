c6d7d8a56da484ccbba65492fc0e19c7dbf1065f
Florian Nücke <florian@nuecke.de>
2020-09-15 15:00:36 +0200

    Cleaned up memory exceptions.

--- src/main/java/li/cil/circuity/vm/device/memory/exception/LoadPageFaultException.java
+++ src/main/java/li/cil/circuity/vm/device/memory/exception/LoadPageFaultException.java
@@ -3,14 +3,7 @@ package li.cil.circuity.vm.device.memory.exception;
 import li.cil.circuity.api.vm.device.memory.MemoryAccessException;
 
 public final class LoadPageFaultException extends MemoryAccessException {
-    private final int address;
-
     public LoadPageFaultException(final int address) {
         super(address);
-        this.address = address;
-    }
-
-    public int getAddress() {
-        return address;
     }
 }
--- src/main/java/li/cil/circuity/vm/device/memory/exception/MisalignedFetchException.java
+++ src/main/java/li/cil/circuity/vm/device/memory/exception/MisalignedFetchException.java
@@ -3,14 +3,7 @@ package li.cil.circuity.vm.device.memory.exception;
 import li.cil.circuity.api.vm.device.memory.MemoryAccessException;
 
 public final class MisalignedFetchException extends MemoryAccessException {
-    private final int address;
-
     public MisalignedFetchException(final int address) {
         super(address);
-        this.address = address;
-    }
-
-    public int getAddress() {
-        return address;
     }
 }

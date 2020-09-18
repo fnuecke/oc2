cdc736f733693b4c36b28ab44e8eeee674b0bc00
Florian Nücke <florian@nuecke.de>
2020-09-19 00:55:23 +0200

    OK

--- src/main/java/li/cil/circuity/api/vm/device/memory/Sizes.java
+++ src/main/java/li/cil/circuity/api/vm/device/memory/Sizes.java
@@ -3,7 +3,7 @@ package li.cil.circuity.api.vm.device.memory;
 public final class Sizes {
     public static final int SIZE_8 = 8;
     public static final int SIZE_16 = 16;
-    public static final int SIZE_32 = 8;
+    public static final int SIZE_32 = 32;
 
     public static final int SIZE_8_LOG2 = 0;
     public static final int SIZE_16_LOG2 = 1;

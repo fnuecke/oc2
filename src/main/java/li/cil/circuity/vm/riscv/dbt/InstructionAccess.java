457933d9a42600a06f09d0fe1d7d3b996d8ba03e
Florian Nücke <florian@nuecke.de>
2020-09-19 00:49:51 +0200

    Removed unused interface.

--- src/main/java/li/cil/circuity/vm/riscv/dbt/InstructionAccess.java
+++
@@ -1,9 +0,0 @@
-package li.cil.circuity.vm.riscv.dbt;
-
-import li.cil.circuity.api.vm.device.memory.MemoryAccessException;
-import li.cil.circuity.vm.riscv.R5CPU;
-
-@FunctionalInterface
-public interface InstructionAccess {
-    R5CPU.TLBEntry fetchPage(final int pc) throws MemoryAccessException;
-}

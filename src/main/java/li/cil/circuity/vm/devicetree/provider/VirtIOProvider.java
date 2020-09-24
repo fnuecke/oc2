1f43d7bc85a014a7a5a706748dc3658f57ede499
Florian Nücke <florian@nuecke.de>
2020-09-24 13:28:47 +0200

    Add name for VirtIO devicetree provider.

--- src/main/java/li/cil/circuity/vm/devicetree/provider/VirtIOProvider.java
+++ src/main/java/li/cil/circuity/vm/devicetree/provider/VirtIOProvider.java
@@ -6,8 +6,15 @@ import li.cil.circuity.api.vm.devicetree.DeviceTree;
 import li.cil.circuity.api.vm.devicetree.DeviceTreePropertyNames;
 import li.cil.circuity.api.vm.devicetree.DeviceTreeProvider;
 
+import java.util.Optional;
+
 public final class VirtIOProvider implements DeviceTreeProvider {
     public static final DeviceTreeProvider INSTANCE = new VirtIOProvider();
+
+    @Override
+    public Optional<String> getName(final Device device) {
+        return Optional.of("virtio");
+    }
 
     @Override
     public void visit(final DeviceTree node, final MemoryMap memoryMap, final Device device) {

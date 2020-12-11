896130189f7f97df09c6290634788bb2cb02dacf
Florian Nücke <florian@nuecke.de>
2020-12-11 05:50:58 +0100

    Add realtime counter that uses MC world game-time ticks.

---
+++ src/main/java/li/cil/oc2/common/vm/device/MinecraftRealTimeCounter.java
@@ -0,0 +1,30 @@
+package li.cil.oc2.common.vm.device;
+
+import li.cil.sedna.api.device.rtc.RealTimeCounter;
+import net.minecraft.world.World;
+
+public final class MinecraftRealTimeCounter implements RealTimeCounter {
+    private static final int TICKS_PER_DAY = 24000;
+    private static final int FREQUENCY = TICKS_PER_DAY;
+
+    private World world;
+
+    public void setWorld(final World world) {
+        this.world = world;
+    }
+
+    @Override
+    public long getTime() {
+        final long ticks = world != null ? world.getGameTime() : 0;
+        final long days = ticks; // / TICKS_PER_DAY
+        final long hours = days * 24;
+        final long minutes = hours * 60;
+        final long seconds = minutes * 60;
+        return seconds; // * FREQUENCY
+    }
+
+    @Override
+    public int getFrequency() {
+        return FREQUENCY;
+    }
+}
---
+++ src/main/java/li/cil/oc2/common/vm/device/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package li.cil.oc2.common.vm.device;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

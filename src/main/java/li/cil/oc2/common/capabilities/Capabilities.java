ba5dedaa2fc36e71dc1ce7ab22a133663c3e3c7a
Florian Nücke <florian@nuecke.de>
2020-12-29 17:33:04 +0100

    Added capability for redstone output, inline registration.

--- src/main/java/li/cil/oc2/common/capabilities/Capabilities.java
+++ src/main/java/li/cil/oc2/common/capabilities/Capabilities.java
@@ -1,8 +1,10 @@
 package li.cil.oc2.common.capabilities;
 
 import li.cil.oc2.api.bus.DeviceBusElement;
+import li.cil.oc2.api.bus.device.capabilities.RedstoneEmitter;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.items.IItemHandler;
@@ -20,9 +22,19 @@ public final class Capabilities {
     @CapabilityInject(DeviceBusElement.class)
     public static Capability<DeviceBusElement> DEVICE_BUS_ELEMENT = null;
 
+    @CapabilityInject(RedstoneEmitter.class)
+    public static Capability<RedstoneEmitter> REDSTONE_EMITTER = null;
+
     ///////////////////////////////////////////////////////////////////
 
     public static void initialize() {
-        DeviceBusElementCapability.register();
+        register(DeviceBusElement.class);
+        register(RedstoneEmitter.class);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+
+    private static <T> void register(final Class<T> type) {
+        CapabilityManager.INSTANCE.register(type, new NullStorage<>(), () -> null);
     }
 }

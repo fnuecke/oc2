40472e3205fe38def4d5cfa8f09d641e5822c2b5
Florian Nücke <florian@nuecke.de>
2021-01-10 10:27:24 +0100

    Static import for readability.

--- src/main/java/li/cil/oc2/client/model/BusCableModel.java
+++ src/main/java/li/cil/oc2/client/model/BusCableModel.java
@@ -12,9 +12,10 @@ import net.minecraftforge.client.model.geometry.IModelGeometry;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+
+import static java.util.Objects.requireNonNull;
 
 public final class BusCableModel implements IModelGeometry<BusCableModel> {
     private static final ResourceLocation BUS_CABLE_STRAIGHT_MODEL = new ResourceLocation(API.MOD_ID, "block/cable_straight");
@@ -30,17 +31,17 @@ public final class BusCableModel implements IModelGeometry<BusCableModel> {
     public IBakedModel bake(final IModelConfiguration owner, final ModelBakery bakery, final Function<RenderMaterial, TextureAtlasSprite> spriteGetter, final IModelTransform modelTransform, final ItemOverrideList overrides, final ResourceLocation modelLocation) {
         final IBakedModel bakedBaseModel = proxy.bake(owner, bakery, spriteGetter, modelTransform, overrides, modelLocation);
         final IBakedModel[] straightModelByAxis = {
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_STRAIGHT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y90), spriteGetter)),
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_STRAIGHT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X90_Y0), spriteGetter)),
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_STRAIGHT_MODEL, modelTransform, spriteGetter))
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_STRAIGHT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y90), spriteGetter)),
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_STRAIGHT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X90_Y0), spriteGetter)),
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_STRAIGHT_MODEL, modelTransform, spriteGetter))
         };
         final IBakedModel[] supportModelByFace = {
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X270_Y0), spriteGetter)), // -y
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X90_Y0), spriteGetter)), // +y
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y180), spriteGetter)), // -z
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, modelTransform, spriteGetter)), // +z
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y90), spriteGetter)), // -x
-                Objects.requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y270), spriteGetter)) // +x
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X270_Y0), spriteGetter)), // -y
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X90_Y0), spriteGetter)), // +y
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y180), spriteGetter)), // -z
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, modelTransform, spriteGetter)), // +z
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y90), spriteGetter)), // -x
+                requireNonNull(bakery.getBakedModel(BUS_CABLE_SUPPORT_MODEL, new ModelTransformComposition(modelTransform, ModelRotation.X0_Y270), spriteGetter)) // +x
         };
 
         return new BusCableBakedModel(bakedBaseModel, straightModelByAxis, supportModelByFace);

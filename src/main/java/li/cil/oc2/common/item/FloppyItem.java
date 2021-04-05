4d6b6551a92988647645cc621b15761dfb96dc66
Florian Nücke <florian@nuecke.de>
2021-04-05 12:33:23 +0200

    Update imports.

--- src/main/java/li/cil/oc2/common/item/FloppyItem.java
+++ src/main/java/li/cil/oc2/common/item/FloppyItem.java
@@ -1,15 +1,6 @@
 package li.cil.oc2.common.item;
 
-import li.cil.oc2.client.item.CustomItemColors;
-import li.cil.oc2.common.Constants;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.DyeColor;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.NonNullList;
-import net.minecraft.world.World;
+import net.minecraft.item.IDyeableArmorItem;
 
 public final class FloppyItem extends AbstractStorageItem implements IDyeableArmorItem {
     public FloppyItem(final int capacity) {

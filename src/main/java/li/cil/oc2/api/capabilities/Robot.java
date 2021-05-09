6aee30c29c0f6cc12b796f90f813b853dc5be56d
Florian Nücke <florian@nuecke.de>
2021-05-09 12:17:59 +0200

    robot doc

--- src/main/java/li/cil/oc2/api/capabilities/Robot.java
+++ src/main/java/li/cil/oc2/api/capabilities/Robot.java
@@ -7,9 +7,26 @@ import net.minecraftforge.items.ItemStackHandler;
  * modules installed in them to interact with them.
  */
 public interface Robot {
+    /**
+     * Provides access to the inventory of the robot.
+     * <p>
+     * This does <em>not</em> include the hardware configuration part of the inventory.
+     *
+     * @return the inventory of the robot.
+     */
     ItemStackHandler getInventory();
 
+    /**
+     * The index of the currently selected slot in the inventory.
+     *
+     * @return the selected slot.
+     */
     int getSelectedSlot();
 
+    /**
+     * Sets the index of the currently selected slot in the inventory.
+     *
+     * @param value the slot to select.
+     */
     void setSelectedSlot(final int value);
 }

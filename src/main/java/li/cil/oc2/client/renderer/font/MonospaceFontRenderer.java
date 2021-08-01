2b62d716db3c44765c008ef9f15a831385ddcbc3
Florian Nücke <florian@nuecke.de>
2021-08-01 18:14:48 +0200

    Better monospace rendering in manual.

--- src/main/java/li/cil/oc2/client/renderer/font/MonospaceFontRenderer.java
+++ src/main/java/li/cil/oc2/client/renderer/font/MonospaceFontRenderer.java
@@ -3,14 +3,18 @@ package li.cil.oc2.client.renderer.font;
 import li.cil.manual.api.prefab.renderer.BitmapFontRenderer;
 import li.cil.manual.api.render.FontRenderer;
 import li.cil.oc2.api.API;
-import li.cil.oc2.common.vm.Terminal;
 import net.minecraft.util.ResourceLocation;
 
-public final class TerminalFontRenderer extends BitmapFontRenderer {
-    public static final FontRenderer INSTANCE = new TerminalFontRenderer();
+public final class MonospaceFontRenderer extends BitmapFontRenderer {
+    public static final FontRenderer INSTANCE = new MonospaceFontRenderer();
 
-    private static final ResourceLocation LOCATION_FONT_TEXTURE = new ResourceLocation(API.MOD_ID, "textures/font/terminus_simple.png");
+    private static final ResourceLocation LOCATION_FONT_TEXTURE = new ResourceLocation(API.MOD_ID, "textures/font/monospace.png");
     private static final String CHARS = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+    @Override
+    public int lineHeight() {
+        return 9;
+    }
 
     @Override
     protected CharSequence getCharacters() {
@@ -24,7 +28,7 @@ public final class TerminalFontRenderer extends BitmapFontRenderer {
 
     @Override
     protected int getResolution() {
-        return 128;
+        return 96;
     }
 
     @Override
@@ -39,11 +43,6 @@ public final class TerminalFontRenderer extends BitmapFontRenderer {
 
     @Override
     protected int charWidth() {
-        return Terminal.CHAR_WIDTH;
-    }
-
-    @Override
-    public int lineHeight() {
-        return Terminal.CHAR_HEIGHT;
+        return 6;
     }
 }

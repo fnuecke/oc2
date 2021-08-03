a3edc7e21601d74f1ac28fc428a4f8f329bfe63f
Florian Nücke <florian@nuecke.de>
2021-08-03 02:51:37 +0200

    Avoid device duplication due to synthetic name device.

--- src/main/java/li/cil/oc2/common/bus/device/rpc/TypeNameRPCDevice.java
+++ src/main/java/li/cil/oc2/common/bus/device/rpc/TypeNameRPCDevice.java
@@ -5,6 +5,7 @@ import li.cil.oc2.api.bus.device.rpc.RPCDevice;
 import li.cil.oc2.api.bus.device.rpc.RPCMethod;
 
 import java.util.List;
+import java.util.Objects;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
@@ -24,5 +25,18 @@ public final class TypeNameRPCDevice implements RPCDevice, ItemDevice {
     @Override
     public List<RPCMethod> getMethods() {
         return emptyList();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final TypeNameRPCDevice that = (TypeNameRPCDevice) o;
+        return typeName.equals(that.typeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(typeName);
     }
 }

4ff68cd390535cce7c2f3d0a07f1698c478c3205
Florian Nücke <florian@nuecke.de>
2022-01-10 19:26:01 +0100

    Reworded some item descriptions trying to make them more concise. Makes tooltips a little less daunting. Also enable description for manual item.

--- src/main/java/li/cil/oc2/common/item/ManualItem.java
+++ src/main/java/li/cil/oc2/common/item/ManualItem.java
@@ -7,12 +7,29 @@ import li.cil.manual.api.prefab.item.AbstractManualItem;
 import li.cil.oc2.client.manual.Manuals;
 import li.cil.oc2.client.manual.ModManualScreenStyle;
 import li.cil.oc2.client.manual.ModManualStyle;
+import li.cil.oc2.common.util.TooltipUtils;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public final class ManualItem extends AbstractManualItem {
     public ManualItem() {
         super(new Properties().tab(ItemGroup.COMMON));
+    }
+
+    ///////////////////////////////////////////////////////////////////
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void appendHoverText(final ItemStack stack, @Nullable final Level level, final List<Component> tooltip, final TooltipFlag flag) {
+        super.appendHoverText(stack, level, tooltip, flag);
+        TooltipUtils.tryAddDescription(stack, tooltip);
     }
 
     ///////////////////////////////////////////////////////////////////

f483f142055329b1215d4fdfdd269d57ddee4773
Florian Nücke <florian@nuecke.de>
2022-01-25 18:24:05 +0100

    Recordify.

--- src/main/java/li/cil/oc2/common/bus/BlockEntityDeviceBusController.java
+++ src/main/java/li/cil/oc2/common/bus/BlockEntityDeviceBusController.java
@@ -13,7 +13,7 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import java.lang.ref.WeakReference;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
+import java.util.Optional;
 
 public final class BlockEntityDeviceBusController extends CommonDeviceBusController {
     private final Runnable onBusChunkLoadedStateChanged = this::scheduleBusScan;
@@ -54,18 +54,18 @@ public final class BlockEntityDeviceBusController extends CommonDeviceBusControl
                 final LevelAccessor elementLevel = blockElement.getLevel();
                 final BlockPos elementPosition = blockElement.getPosition();
                 if (elementLevel != null) {
-                    newTrackedChunks.add(new TrackedChunk(elementLevel, elementPosition));
-                    newTrackedChunks.add(new TrackedChunk(elementLevel, elementPosition.relative(Direction.NORTH)));
-                    newTrackedChunks.add(new TrackedChunk(elementLevel, elementPosition.relative(Direction.EAST)));
-                    newTrackedChunks.add(new TrackedChunk(elementLevel, elementPosition.relative(Direction.SOUTH)));
-                    newTrackedChunks.add(new TrackedChunk(elementLevel, elementPosition.relative(Direction.WEST)));
+                    newTrackedChunks.add(TrackedChunk.of(elementLevel, elementPosition));
+                    newTrackedChunks.add(TrackedChunk.of(elementLevel, elementPosition.relative(Direction.NORTH)));
+                    newTrackedChunks.add(TrackedChunk.of(elementLevel, elementPosition.relative(Direction.EAST)));
+                    newTrackedChunks.add(TrackedChunk.of(elementLevel, elementPosition.relative(Direction.SOUTH)));
+                    newTrackedChunks.add(TrackedChunk.of(elementLevel, elementPosition.relative(Direction.WEST)));
                 }
             }
         }
 
         // Do not track the chunk the controller itself is in -- this is unneeded because
         // we expect the controller to be disposed if its chunk is unloaded.
-        newTrackedChunks.remove(new TrackedChunk(level, blockEntity.getBlockPos()));
+        newTrackedChunks.remove(TrackedChunk.of(level, blockEntity.getBlockPos()));
 
         final HashSet<TrackedChunk> removedChunks = new HashSet<>(trackedChunks);
         removedChunks.removeAll(newTrackedChunks);
@@ -83,44 +83,29 @@ public final class BlockEntityDeviceBusController extends CommonDeviceBusControl
 
     private void addListeners(final Collection<TrackedChunk> trackedChunks) {
         for (final TrackedChunk trackedChunk : trackedChunks) {
-            final LevelAccessor level = trackedChunk.level.get();
-            if (level != null) {
+            trackedChunk.tryGetLevel().ifPresent(level -> {
                 ServerScheduler.scheduleOnLoad(level, trackedChunk.position, onBusChunkLoadedStateChanged);
                 ServerScheduler.scheduleOnUnload(level, trackedChunk.position, onBusChunkLoadedStateChanged);
-            }
+            });
         }
     }
 
     private void removeListeners(final Collection<TrackedChunk> trackedChunks) {
         for (final TrackedChunk trackedChunk : trackedChunks) {
-            final LevelAccessor level = trackedChunk.level.get();
-            if (level != null) {
+            trackedChunk.tryGetLevel().ifPresent(level -> {
                 ServerScheduler.cancelOnLoad(level, trackedChunk.position, onBusChunkLoadedStateChanged);
                 ServerScheduler.cancelOnUnload(level, trackedChunk.position, onBusChunkLoadedStateChanged);
-            }
+            });
         }
     }
 
-    private static final class TrackedChunk {
-        public final WeakReference<LevelAccessor> level;
-        public final ChunkPos position;
-
-        private TrackedChunk(final LevelAccessor level, final BlockPos position) {
-            this.level = new WeakReference<>(level);
-            this.position = new ChunkPos(position);
+    private record TrackedChunk(WeakReference<LevelAccessor> level, ChunkPos position) {
+        public static TrackedChunk of(final LevelAccessor level, final BlockPos position) {
+            return new TrackedChunk(new WeakReference<>(level), new ChunkPos(position));
         }
 
-        @Override
-        public boolean equals(final Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            final TrackedChunk that = (TrackedChunk) o;
-            return level.equals(that.level) && position.equals(that.position);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(level, position);
+        public Optional<LevelAccessor> tryGetLevel() {
+            return Optional.ofNullable(level.get());
         }
     }
 }

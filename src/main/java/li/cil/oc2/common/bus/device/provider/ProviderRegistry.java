0564ce614e5cd5dc824a837a30a7395f14f834ac
Florian Nücke <florian@nuecke.de>
2022-02-13 16:05:33 +0100

    Consistency.

--- src/main/java/li/cil/oc2/common/bus/device/provider/ProviderRegistry.java
+++ src/main/java/li/cil/oc2/common/bus/device/provider/ProviderRegistry.java
@@ -4,15 +4,11 @@ package li.cil.oc2.common.bus.device.provider;
 
 import li.cil.oc2.api.bus.device.provider.BlockDeviceProvider;
 import li.cil.oc2.api.bus.device.provider.ItemDeviceProvider;
-import li.cil.oc2.common.bus.device.provider.block.BlockEntityCapabilityDeviceProvider;
-import li.cil.oc2.common.bus.device.provider.item.*;
-import li.cil.oc2.common.bus.device.rpc.block.*;
 import li.cil.oc2.common.util.RegistryUtils;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.RegistryBuilder;
 
-import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
 public final class ProviderRegistry {
@@ -27,39 +23,7 @@ public final class ProviderRegistry {
     ///////////////////////////////////////////////////////////////////
 
     public static void initialize() {
-        registerBlockDeviceProviders(BLOCK_DEVICE_PROVIDERS::register);
-        registerItemDeviceProviders(ITEM_DEVICE_PROVIDERS::register);
-    }
-
-    public static void registerBlockDeviceProviders(final BiConsumer<String, Supplier<BlockDeviceProvider>> registry) {
-        registry.accept("block", BlockStateObjectDeviceProvider::new);
-        registry.accept("block_entity", BlockEntityObjectDeviceProvider::new);
-
-        registry.accept("block_entity/capability", BlockEntityCapabilityDeviceProvider::new);
-        registry.accept("energy_storage", EnergyStorageBlockDeviceProvider::new);
-        registry.accept("fluid_handler", FluidHandlerBlockDeviceProvider::new);
-        registry.accept("item_handler", ItemHandlerBlockDeviceProvider::new);
-    }
-
-    public static void registerItemDeviceProviders(final BiConsumer<String, Supplier<ItemDeviceProvider>> registry) {
-        registry.accept("memory", MemoryItemDeviceProvider::new);
-        registry.accept("hard_drive", HardDriveItemDeviceProvider::new);
-        registry.accept("hard_drive_custom", HardDriveWithExternalDataItemDeviceProvider::new);
-        registry.accept("flash_memory", FlashMemoryItemDeviceProvider::new);
-        registry.accept("flash_memory_custom", FlashMemoryWithExternalDataItemDeviceProvider::new);
-        registry.accept("redstone_interface_card", RedstoneInterfaceCardItemDeviceProvider::new);
-        registry.accept("network_interface_card", NetworkInterfaceCardItemDeviceProvider::new);
-        registry.accept("network_tunnel_card", NetworkTunnelCardItemDeviceProvider::new);
-        registry.accept("file_import_export_card", FileImportExportCardItemDeviceProvider::new);
-        registry.accept("sound_card", SoundCardItemDeviceProvider::new);
-
-        registry.accept("inventory_operations_module", InventoryOperationsModuleDeviceProvider::new);
-        registry.accept("block_operations_module", BlockOperationsModuleDeviceProvider::new);
-        registry.accept("network_tunnel_module", NetworkTunnelModuleItemDeviceProvider::new);
-
-        registry.accept("item_stack/capability", ItemStackCapabilityDeviceProvider::new);
-        registry.accept("energy_storage", EnergyStorageItemDeviceProvider::new);
-        registry.accept("fluid_handler", FluidHandlerItemDeviceProvider::new);
-        registry.accept("item_handler", ItemHandlerItemDeviceProvider::new);
+        Providers.registerBlockDeviceProviders(BLOCK_DEVICE_PROVIDERS::register);
+        Providers.registerItemDeviceProviders(ITEM_DEVICE_PROVIDERS::register);
     }
 }
--- src/main/java/li/cil/oc2/common/bus/device/provider/Providers.java
+++ src/main/java/li/cil/oc2/common/bus/device/provider/Providers.java
@@ -4,7 +4,13 @@ package li.cil.oc2.common.bus.device.provider;
 
 import li.cil.oc2.api.bus.device.provider.BlockDeviceProvider;
 import li.cil.oc2.api.bus.device.provider.ItemDeviceProvider;
+import li.cil.oc2.common.bus.device.provider.block.BlockEntityCapabilityDeviceProvider;
+import li.cil.oc2.common.bus.device.provider.item.*;
+import li.cil.oc2.common.bus.device.rpc.block.*;
 import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
 
 public final class Providers {
     public static IForgeRegistry<BlockDeviceProvider> blockDeviceProviderRegistry() {
@@ -13,5 +19,37 @@ public final class Providers {
 
     public static IForgeRegistry<ItemDeviceProvider> itemDeviceProviderRegistry() {
         return ProviderRegistry.ITEM_DEVICE_PROVIDER_REGISTRY.get();
+    }
+
+    public static void registerBlockDeviceProviders(final BiConsumer<String, Supplier<BlockDeviceProvider>> registry) {
+        registry.accept("block", BlockStateObjectDeviceProvider::new);
+        registry.accept("block_entity", BlockEntityObjectDeviceProvider::new);
+
+        registry.accept("block_entity/capability", BlockEntityCapabilityDeviceProvider::new);
+        registry.accept("energy_storage", EnergyStorageBlockDeviceProvider::new);
+        registry.accept("fluid_handler", FluidHandlerBlockDeviceProvider::new);
+        registry.accept("item_handler", ItemHandlerBlockDeviceProvider::new);
+    }
+
+    public static void registerItemDeviceProviders(final BiConsumer<String, Supplier<ItemDeviceProvider>> registry) {
+        registry.accept("memory", MemoryItemDeviceProvider::new);
+        registry.accept("hard_drive", HardDriveItemDeviceProvider::new);
+        registry.accept("hard_drive_custom", HardDriveWithExternalDataItemDeviceProvider::new);
+        registry.accept("flash_memory", FlashMemoryItemDeviceProvider::new);
+        registry.accept("flash_memory_custom", FlashMemoryWithExternalDataItemDeviceProvider::new);
+        registry.accept("redstone_interface_card", RedstoneInterfaceCardItemDeviceProvider::new);
+        registry.accept("network_interface_card", NetworkInterfaceCardItemDeviceProvider::new);
+        registry.accept("network_tunnel_card", NetworkTunnelCardItemDeviceProvider::new);
+        registry.accept("file_import_export_card", FileImportExportCardItemDeviceProvider::new);
+        registry.accept("sound_card", SoundCardItemDeviceProvider::new);
+
+        registry.accept("inventory_operations_module", InventoryOperationsModuleDeviceProvider::new);
+        registry.accept("block_operations_module", BlockOperationsModuleDeviceProvider::new);
+        registry.accept("network_tunnel_module", NetworkTunnelModuleItemDeviceProvider::new);
+
+        registry.accept("item_stack/capability", ItemStackCapabilityDeviceProvider::new);
+        registry.accept("energy_storage", EnergyStorageItemDeviceProvider::new);
+        registry.accept("fluid_handler", FluidHandlerItemDeviceProvider::new);
+        registry.accept("item_handler", ItemHandlerItemDeviceProvider::new);
     }
 }

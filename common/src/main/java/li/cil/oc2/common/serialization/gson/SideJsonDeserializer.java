38d90edca5838de8c844832c91ec8a439579ce94
Florian Nücke <florian@nuecke.de>
2026-08-14 18:35:40 +0200

    Explicit out of bounds handling for side deserializer

--- common/src/main/java/li/cil/oc2/common/serialization/gson/SideJsonDeserializer.java
+++ common/src/main/java/li/cil/oc2/common/serialization/gson/SideJsonDeserializer.java
@@ -10,12 +10,19 @@ import li.cil.oc2.api.util.Side;
 import java.lang.reflect.Type;
 
 public final class SideJsonDeserializer implements JsonDeserializer<Side> {
+    private static final Side[] SIDES = Side.values();
+
     @Override
     public Side deserialize(final JsonElement json, final Type typeOfT, final JsonDeserializationContext context) throws JsonParseException {
         if (json.isJsonPrimitive()) {
             final JsonPrimitive jsonPrimitive = json.getAsJsonPrimitive();
             if (jsonPrimitive.isNumber()) {
-                return Side.values()[jsonPrimitive.getAsNumber().intValue()];
+                final int ordinal = jsonPrimitive.getAsNumber().intValue();
+                if (ordinal < 0 || ordinal >= SIDES.length) {
+                    throw new JsonParseException("side out of range: " + ordinal
+                            + " (expected 0 to " + (SIDES.length - 1) + ")");
+                }
+                return SIDES[ordinal];
             }
         }
 

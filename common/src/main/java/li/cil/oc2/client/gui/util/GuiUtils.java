8f0b06e54b7a9bb5b5a287451fcde1ad678e4337
Florian Nücke <florian@nuecke.de>
2026-08-15 11:39:15 +0200

    Lazy init to avoid unfortunate static class init running before registry setup

--- common/src/main/java/li/cil/oc2/client/gui/util/GuiUtils.java
+++ common/src/main/java/li/cil/oc2/client/gui/util/GuiUtils.java
@@ -7,7 +7,6 @@ import li.cil.oc2.api.bus.device.DeviceTypes;
 import li.cil.oc2.client.gui.widget.Sprite;
 import li.cil.oc2.common.container.DeviceTypeSlotItemHandler;
 import li.cil.oc2.common.util.TooltipUtils;
-import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -15,20 +14,29 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.Slot;
 
+import javax.annotation.Nullable;
 import java.util.*;
 
 import static li.cil.oc2.common.util.TranslationUtils.text;
 
 public final class GuiUtils {
-    private static final Map<DeviceType, Component> WARNING_BY_DEVICE_TYPE = Util.make(() -> {
-        final HashMap<DeviceType, Component> map = new HashMap<>();
+    @Nullable
+    private static Map<DeviceType, Component> warningByDeviceType;
 
-        map.put(DeviceTypes.FLASH_MEMORY, text("tooltip.{mod}.flash_memory_missing"));
-        map.put(DeviceTypes.MEMORY, text("tooltip.{mod}.memory_missing"));
-        map.put(DeviceTypes.HARD_DRIVE, text("tooltip.{mod}.hard_drive_missing"));
+    private static Map<DeviceType, Component> getWarningByDeviceType() {
+        // Lazy init to make sure init is done and the type keys are set.
+        if (warningByDeviceType == null) {
+            final HashMap<DeviceType, Component> map = new HashMap<>();
 
-        return map;
-    });
+            map.put(DeviceTypes.FLASH_MEMORY, text("tooltip.{mod}.flash_memory_missing"));
+            map.put(DeviceTypes.MEMORY, text("tooltip.{mod}.memory_missing"));
+            map.put(DeviceTypes.HARD_DRIVE, text("tooltip.{mod}.hard_drive_missing"));
+
+            warningByDeviceType = map;
+        }
+
+        return warningByDeviceType;
+    }
 
     private static final int SLOT_SIZE = 18;
     private static final int DEVICE_INFO_ICON_SIZE = 28;
@@ -46,7 +54,7 @@ public final class GuiUtils {
     }
 
     public static <TContainer extends AbstractContainerMenu> void renderMissingDeviceInfoTooltip(final GuiGraphics graphics, final AbstractContainerScreen<TContainer> screen, final int mouseX, final int mouseY, final DeviceType type) {
-        renderMissingDeviceInfoTooltip(graphics, screen, mouseX, mouseY, type, Objects.requireNonNull(WARNING_BY_DEVICE_TYPE.get(type)));
+        renderMissingDeviceInfoTooltip(graphics, screen, mouseX, mouseY, type, Objects.requireNonNull(getWarningByDeviceType().get(type)));
     }
 
     public static <TContainer extends AbstractContainerMenu> void renderMissingDeviceInfoTooltip(final GuiGraphics graphics, final AbstractContainerScreen<TContainer> screen, final int mouseX, final int mouseY, final DeviceType type, final Component tooltip) {
